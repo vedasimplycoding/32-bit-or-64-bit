24c4bbd6994860ab2f73381e88b1f692b280326b
Veda Simply Coding <90026881+vedasimplycoding@users.noreply.github.com>
2021-10-07 20:49:37 +0530

    Update 32-bit or 64-bit .cmd

--- 32-bit or 64-bit .cmd
+++ 32-bit or 64-bit .cmd
@@ -1,6 +1,3 @@
 @echo off
 systeminfo
-(echo.& echo ====================================== & echo. & choice /n /c YN /m "Would you like to visit my YouTube Channel [Y,N]?" & if errorlevel 2 pause) || (echo The connection to the server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
-explorer "https://bit.ly/VedaSimplyCodingYT"&goto halt
 pause
-start "" https://bit.ly/VedaSimplyCodingYT
